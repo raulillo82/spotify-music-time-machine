2c4026ebe28fb6543267183d48f309af0df5bad9
Raúl Osuna <rosuna@suse.com>
2023-12-03 17:27:34 +0100

    Fix search to also filter by artist for avoiding undesired results

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ billboard_page = response.text
 soup = BeautifulSoup(billboard_page, "html.parser")
 
 print(f"Scrapping top 100 tracks on {date} from billboard.com...")
-tracks = [track.getText().strip() for track
-          in soup.select(selector="li>ul>li>h3")]
+songs = [track.getText().strip() for track
+         in soup.select(selector="li>ul>li>h3")]
 
 print(f"Scrapping track artists from previous list...")
-song_artists = [artist.getText().strip() for artist
-               in soup.select(selector="li>ul>li>span.c-label.a-no-trucate")]
+artists = [artist.getText().strip() for artist
+           in soup.select(selector="li>ul>li>span.c-label.a-no-trucate")]
+
+tracks = list(zip(songs, artists))
 
 #Connection to Spotify
 scope = "playlist-modify-public"
@@ -35,10 +37,12 @@ sp = spotipy.Spotify(
             redirect_uri=redirect_uri,
             scope=scope))
 
+#year = date.split("-")[0]
+
 #Get Spotify URIs:
 print(f"Fetching the songs from Spotify...")
-spotify_songs_URIs = [sp.search(q=song, limit=1, type="track")["tracks"]["items"][0]["uri"]
-                      for song in tracks]
+spotify_songs_URIs = [sp.search(q=f"track: {song} artist: {artist}", limit=1, type="track")["tracks"]["items"][0]["uri"]
+                      for (song, artist) in tracks]
 
 #Get Spotify user, needed to create the list:
 user = sp.current_user()["id"]
@@ -52,4 +56,4 @@ print(f"Adding fetched tracks from {date} to playlist {playlist_name}...")
 result = sp.user_playlist_add_tracks(user, playlist["id"], spotify_songs_URIs)
 if result:
     print("Songs successfully added:")
-    print(list(zip(tracks, song_artists)))
+    print(tracks)
